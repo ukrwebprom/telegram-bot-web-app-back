8a452b4a71404987725c7d84ba6e32293380ff31
ukrwebprom <ukrwebprom@gmail.com>
2023-05-27 15:47:41 +0300

    menu commands updated

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 const {BOT_TOKEN} = process.env;
+const PAGE_URL = 'https://pochtiennykh-bot.onrender.com';
+const BEHANCE = 'https://www.behance.net/ukrwebprom';
+const GIT = 'https://github.com/ukrwebprom';
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(BOT_TOKEN, {polling: true});
@@ -9,6 +12,54 @@ const bot = new TelegramBot(BOT_TOKEN, {polling: true});
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
     console.log(msg);
+    switch(msg.text.toLowerCase()) {
+      case '/start':
+        bot.sendMessage(chatId, 'Hi. Nice to meet you 🤝');
+        break;
+      case '/about':
+        bot.sendMessage(chatId, 'sure..',{
+          reply_markup: {
+            inline_keyboard: [
+              [{text: 'Open about page', web_app: {url:PAGE_URL}}]
+            ]
+          }
+        });
+        break;
+      case '/cv':
+        bot.sendMessage(chatId, 'Here it is');
+        bot.sendDocument(chatId, './files/Yuriy-Pochtiennykh-Junior-Front-end-developer.pdf');
+        break;
+      case '/phone':
+        bot.sendContact(chatId, '+380963336533', 'Yurii', {last_name:'Pochtiennykh'});
+        break;
+      case '/portfolio':
+        bot.sendMessage(chatId, 'Here is my design works. The portfolio shows works made at different times and from different areas - print design, games, web, 3D...',{
+          reply_markup: {
+            inline_keyboard: [
+              [{text: 'Open my Behance', web_app: {url:BEHANCE}}]
+            ]
+          }
+        });
+        break;
+        case '/git':
+          bot.sendMessage(chatId, 'My Github',{
+            reply_markup: {
+              inline_keyboard: [
+                [{text: 'Open my Git', web_app: {url:GIT}}]
+              ]
+            }
+          });
+          break;
+          case 'я тебя люблю':
+            bot.sendMessage(chatId, 'Я тебя тоже ❤️');
+            break;
+          case 'дука':
+            bot.sendMessage(chatId, 'Я здесь');
+            break;
+      default:
+        bot.sendMessage(chatId, 'Hmmm...');
+    }
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Ровач');
+  
+
 });
