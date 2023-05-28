a0d4dcfa313ebd58217f187c14ba4a88b8838c48
ukrwebprom <ukrwebprom@gmail.com>
2023-05-29 00:27:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.post('/webhook', (req, res) => {
-  res.status(200).json({ success: true });;
+  console.log('req:', req.body);
+  res.status(200).json({ success: true });
 });
 
 app.listen(PORT, () => {
