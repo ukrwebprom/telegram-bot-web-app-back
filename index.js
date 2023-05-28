c67d6c1a2700c50197bc4498811a337fd0802114
ukrwebprom <ukrwebprom@gmail.com>
2023-05-28 11:24:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ bot.on('text', (msg) => {
 });
 
 bot.start();
+console.log(bot.getFile('BQACAgIAAxkBAAEhvzBkcwY5N3ZtfoIaf0azZrjUrqHHVAACAjEAAhw7mUsIhVoo5jyX1i8E'));
 bot.getWebhookInfo().then((res) => console.log(res))
 
 /* bot.on('message', (msg) => {
